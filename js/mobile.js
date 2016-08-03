1f63f3dbd66efaa858c26ab79f918e3ca0c56a13
Tom Golden <tbjgolden@gmail.com>
2016-08-03 13:48:33 -0700

    Mobiles get parallax

---
+++ js/mobile.js
@@ -0,0 +1,5 @@
+window.isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
+
+if (isMobile) {
+    $('.caption').css('font-size', 'smaller');
+}
